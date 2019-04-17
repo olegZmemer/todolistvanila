c5ee8784629ecaf3b327dd76f1fb0c4b5716cd88
olegZmemer <olegZmemer2@gmail.com>
2019-04-18 01:51:53 +0300

    version 1.0

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 (function todoList() {
-    let addForm = document.querySelector('#add-task-form');
-    removeButton = document.querySelector('.task-remove'),
+    let addForm = document.querySelector('#add-task-form'),
+        removeButton = document.querySelector('.task-remove'),
         taskNameInput = document.querySelector('#add-task'),
-        checkTaskButtons = document.querySelectorAll('.check-task-button'),
-        tasks = [],
-        id = 0,
         dataObject = {
-            tasks: tasks
+            tasks: [],
+            id: 0
         },
         serializedDataObject = JSON.stringify(dataObject);
+    if (localStorage.getItem('mainData')) {
         (function loadStorage() {
             let data = JSON.parse(localStorage.getItem("mainData"));
             let tasks = data.tasks;
+            dataObject.id = JSON.parse(localStorage.getItem('mainData')).id;
+            dataObject.tasks = JSON.parse(localStorage.getItem('mainData')).tasks;
+            data.id = dataObject.id;
             tasks.forEach(function (task) {
                 let taskBlock = document.createElement('div');
                 taskBlock.classList.add('task-block');
                 taskBlock.innerHTML = `
-            <div class="task-info">
-                <label><input type="checkbox" class="check-task-button">${task.name}</label>
-                <div class="task-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-            </div>
-            <button class="task-remove">X</button>
-        `;
+                <div class="task-info">
+                    <label><input type="checkbox" class="check-task-button">${task.name}</label>
+                    <div class="task-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                </div>
+                <button class="task-remove">X</button>
+            `;
                 taskBlock.id = task.id;
                 document.querySelector('.active-tasks-list').appendChild(taskBlock);
                 if (task.isChecked) {
@@ -31,15 +33,16 @@
                 checkTask(taskBlock, task);
             });
         })();
+    }
 
     function addTask() {
         let task = {};
-        id++;
-        task.id = id;
+        dataObject.id++;
+        task.id = dataObject.id;
         task.name = taskNameInput.value;
         task.isChecked = false;
 
-        tasks.push(task);
+        dataObject.tasks.push(task);
         saveToLocaleStorage();
         let taskBlock = document.createElement('div');
         taskBlock.classList.add('task-block');
@@ -50,39 +53,30 @@
             </div>
             <button class="task-remove">X</button>
         `;
-        taskBlock.id = id;
+        taskBlock.id = dataObject.id;
         document.querySelector('.active-tasks-list').appendChild(taskBlock);
         taskNameInput.value = '';
         taskNameInput.focus();
-        console.log(tasks);
-        console.log(serializedDataObject);
-        // taskBlock.querySelector('.check-task-button').addEventListener('change', function (e) {
-        //     tasks.forEach(function (element) {
-        //         if (e.target.closest('.task-block').id == element.id) {
-        //             element.isChecked = true;
-        //             let taskBlock = document.getElementById(element.id);
-        //             taskBlock.querySelector('.check-task-button').setAttribute('disabled', true);
-        //             document.querySelector('.checked-tasks-list').appendChild(taskBlock);
-        //             saveToLocaleStorage();
-        //         }
-        //     })
-        // })
+        console.log(localStorage.getItem('mainData'));
         checkTask(taskBlock, task);
     }
-    function checkTask(taskBlock, task){
+
+    function checkTask(taskBlock, task) {
         taskBlock.querySelector('.check-task-button').addEventListener('change', function (e) {
-                if (e.target.closest('.task-block').id == task.id) {
-                    task.isChecked = true;
-                    let taskBlock = document.getElementById(task.id);
-                    taskBlock.querySelector('.check-task-button').setAttribute('disabled', true);
-                    document.querySelector('.checked-tasks-list').appendChild(taskBlock);
-                    saveToLocaleStorage();
-                }
-            })
+            if (e.target.closest('.task-block').id == task.id) {
+                task.isChecked = true;
+                let taskBlock = document.getElementById(task.id);
+                taskBlock.querySelector('.check-task-button').setAttribute('disabled', true);
+                document.querySelector('.checked-tasks-list').appendChild(taskBlock);
+                saveToLocaleStorage();
+            }
+        })
     }
+
     function saveToLocaleStorage() {
         serializedDataObject = JSON.stringify(dataObject);
         return localStorage.setItem('mainData', serializedDataObject);
     }
     addForm.addEventListener('submit', addTask);
+    //localStorage.clear();
 })()
